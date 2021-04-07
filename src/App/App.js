9c461a19656039c5eed12156fc06d1fbd8bd81ba
Owen Hallgren <owenhallgren@gmail.com>
2021-04-07 10:28:17 -0600

    Displayed form

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { resFetch } from '../apiCall';
 import ResDisplay from '../ResDisplay/ResDisplay';
+import Form from '../Form/Form';
 
 
 
@@ -17,7 +18,9 @@ class App extends Component {
      resFetch().then(data => this.setState({ reservations: data }))
   }
    
-      
+  addRes = (newRes) => {
+    this.setState({ reservations: [...this.state.reservations, newRes]})
+  }   
   
 
   render() {
@@ -25,7 +28,7 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form addRes={this.addRes}/>
         </div>
         {this.state.reservations.length && <div className='resy-container'>
           <ResDisplay reservations={this.state.reservations}/>
